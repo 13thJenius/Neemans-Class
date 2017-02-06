bed42d0e998ec851f911a0f4ee52c98f893a7fce
cshank81 <cabshanklin@gmail.com>
2017-02-06 11:02:10 -0600

    Update Ch10Lab.java

--- Ch10Lab.java
+++ Ch10Lab.java
@@ -1,10 +1,18 @@
+import java.util.Scanner
+
 package apComputer;
 
 public class AustinHeatonLab10{
 	
 	public static void main (String [] args){
-		Hero myHero = new Hero("Steelheart");
-		System.out.println(myHero.getName());
+		Scanner input = new Scanner(System.in);
+		System.out.println("Enter a hero: ")
+		String name = input.nextLine();
+		System.out.rpintln("Enter a Weapon: ");
+		String weapon = input.nextLine();
+		Hero myHero = new Hero(name);
+		
+		System.out.println(myHero.getStats());
 	}
 }
 
@@ -20,20 +28,27 @@ class Hero{
 	}
 }
 
-class Powers extends Hero{
+class Powers{
 	String powers;
 	String name;
 	
 	public Powers(String name, String powers){
-		this.name = name;
+		super(name);
 		this.powers = powers;
 	}
 }
 
-class Weapons{
-	String weapon = "";
-	public Weapons(String weapon){
-		this.weapon = weapon;
+class AlterEgo extends Hero{
+	String Alter = "";
+	 public static String flipString(String param)
+   {
+      Alter = "";
+      for( int i = param.length()-1; i >= 0; i--)
+      {
+         Alter += param.charAt(i);
+      }
+      return Alter;
+
 	}
 }
 
