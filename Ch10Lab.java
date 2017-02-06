806cecc018b41b6a0b64b463e4e7b5818e5a86da
shockmystic <na.aquamarine@gmail.com>
2017-02-06 10:35:36 -0600

    Update Ch10Lab.java

--- Ch10Lab.java
+++ Ch10Lab.java
@@ -1,26 +1,28 @@
-package apComputer;
-
 public class AustinHeatonLab10{
 	
 	public static void main (String [] args){
+		Hero myHero = new Hero(Steelheart);
 		
 	}
 }
 
-public class Hero(){
+public class Hero(String name){
+	String name;
 	public Hero{
-		
+		this.name = name;
 	}
 }
 
-public class Powers extends Hero(){
+public class Powers extends Hero(String name, String powers){
+	String powers;
 	public Powers{
-		
+		this.powers = powers;
 	}
 }
 
-public class Weapons(){
+public class Weapons(String weapon){
+	String weapon;
 	public Weapons{
-		
+		this.weapon = weapon;
 	}
 }
