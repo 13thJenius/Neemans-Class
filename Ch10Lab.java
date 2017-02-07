7cb37d62f777a59b3015c3e528aa8584a21a4f80
shockmystic <na.aquamarine@gmail.com>
2017-02-07 10:19:45 -0600

    Update Ch10Lab.java
    This one uses a name and a power for a hero.

--- Ch10Lab.java
+++ Ch10Lab.java
@@ -1,58 +1,40 @@
-import java.util.Scanner;
-
-//package apComputer;
-
-public class Ch10Lab{
+public class AustinHeatonLab10{
 	
 	public static void main (String [] args){
-		Scanner input = new Scanner(System.in);
-		System.out.println("Enter a hero: ");
-		String name = input.nextLine();
-		System.out.println("Enter a Weapon: ");
-		String weapon = input.nextLine();
-		Hero myHero = new Hero(name);
-		
-		System.out.println(myHero.getStats());
+		Super mySuper = new Super("Mr. Neeman", "Hero");
+		System.out.println(mySuper.getProfile());
 	}
 }
 
-class Hero{
+class Person{
 	String name;
 	
-	public Hero(String name){
+	public Person(String name){
 		this.name = name;
 	}
-	
-	public String getName(){
-		return name;
-	}
-   
-   public String getStats(){
-      return name;
-   }
-}
+}//close class Person
 
 class Powers{
 	String powers;
-	String name;
 	
-	public Powers(String name, String powers){
-		super(name);//this is something you can only use for inheritance, not composition, right? it won't compile.
+	public Powers(String powers){
 		this.powers = powers;
 	}
-}
+}//close class Powers
 
-class AlterEgo extends Hero{
-	String Alter = "";
-	 public static String flipString(String param)
-   {
-      Alter = "";
-      for( int i = param.length()-1; i >= 0; i--)
-      {
-         Alter += param.charAt(i);
-      }
-      return Alter;
-
+class Super extends Person{
+	String sideOf;
+	Powers myPower = new Powers("Hyper Cognitive Ability");
+	
+	public Super(String name, String Super) {
+		super(name);
+		Super.toLowerCase();
+		sideOf = Super;
+		
 	}
+	public String getProfile(){
+		return "Your Hero is: " + name + "\n" + "He has the power of: " + myPower.powers;
+	}
+	
 }
 
